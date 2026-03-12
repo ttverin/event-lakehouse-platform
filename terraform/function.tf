c2412b732fab9f57e5e28941b86ea8a8fec3ad75
Teemu Tverin <teemu.tverin@zuhlke.com>
2026-03-12 13:53:25 +0100

    feat: add azure function app and nodejs function

--- terraform/function.tf
+++ terraform/function.tf
@@ -1,4 +1,4 @@
-# Function App Storage (different from Data Lake)
+# Function App Storage
 resource "azurerm_storage_account" "function_storage" {
   name                     = "funcstorage${random_id.suffix.hex}"
   resource_group_name      = azurerm_resource_group.main.name
@@ -12,7 +12,7 @@ resource "random_id" "suffix" {
   byte_length = 4
 }
 
-# App Service Plan (Consumption = serverless)
+# App Service Plan
 resource "azurerm_app_service_plan" "function_plan" {
   name                = "func-plan-${random_id.suffix.hex}"
   location            = azurerm_resource_group.main.location
@@ -25,14 +25,13 @@ resource "azurerm_app_service_plan" "function_plan" {
 }
 
 # Function App
-resource "azurerm_function_app" "event_ingest" {
+resource "azurerm_linux_function_app" "event_ingest" {
   name                       = "event-ingest-${random_id.suffix.hex}"
   location                   = azurerm_resource_group.main.location
   resource_group_name        = azurerm_resource_group.main.name
-  app_service_plan_id        = azurerm_app_service_plan.function_plan.id
+  service_plan_id            = azurerm_app_service_plan.function_plan.id
   storage_account_name       = azurerm_storage_account.function_storage.name
   storage_account_access_key = azurerm_storage_account.function_storage.primary_access_key
-  version                    = "~4"
   https_only                 = true
 
   site_config {

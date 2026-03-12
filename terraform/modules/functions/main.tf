1559fdf41a1288f4513117d253c9eb877224817c
Teemu Tverin <teemu.tverin@zuhlke.com>
2026-03-12 17:04:31 +0100

    feat: add events container for ticketmaster api outputs

--- terraform/modules/functions/main.tf
+++ terraform/modules/functions/main.tf
@@ -6,6 +6,12 @@ resource "azurerm_service_plan" "plan" {
   sku_name            = "Y1"
 }
 
+resource "azurerm_storage_container" "events" {
+  name                  = "events"
+  storage_account_name    = var.storage_account
+  container_access_type = "private"
+}
+
 resource "azurerm_linux_function_app" "func" {
   name                = var.function_name
   location            = var.location
@@ -23,6 +29,8 @@ resource "azurerm_linux_function_app" "func" {
 
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME = "node"
+    DATALAKE_ACCOUNT_NAME    = var.storage_account
+    EVENTS_CONTAINER_NAME    = azurerm_storage_container.events.name
   }
 }
 
